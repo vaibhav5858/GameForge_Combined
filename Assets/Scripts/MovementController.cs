1108ad9ef708e76c6528f83601f153ffb160bab7
vaibhav5858 <vaibhav58.yadav@gmail.com>
2025-04-26 20:55:50 +0530

    changes

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public new Rigidbody2D rigidbody { get; private set; }
-    private Vector2 direction = Vector2.down;
+    private Vector2 direction = Vector2.zero;
     public float speed = 5f;
 
     public KeyCode inputUp = KeyCode.W;
     public KeyCode inputDown = KeyCode.S;
-
     public KeyCode inputLeft = KeyCode.A;
     public KeyCode inputRight = KeyCode.D;
-
-
 
     private void Awake()
     {
@@ -23,35 +20,33 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(inputUp))
-        {
-            SetDirection(Vector2.up);
-        } else if (Input.GetKeyDown(inputDown))
-        {
-            SetDirection(Vector2.down);
-        } else if (Input.GetKeyDown(inputLeft))
-        {
-            SetDirection(Vector2.left);
-        } else if (Input.GetKeyDown(inputRight))
-        {
-            SetDirection(Vector2.right);
-        } else
-        {
-            SetDirection(Vector2.zero);
-        }
-    }
+        direction = Vector2.zero; // Reset direction every frame
 
+        if (Input.GetKey(inputUp))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(inputDown))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(inputLeft))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(inputRight))
+        {
+            direction += Vector2.right;
+        }
+
+        direction = direction.normalized; // Prevent faster diagonal movement
+    }
 
     private void FixedUpdate()
     {
         Vector2 position = rigidbody.position;
         Vector2 translation = direction * speed * Time.fixedDeltaTime;
-        
-        rigidbody.MovePosition(position + translation);
-    }
 
-    private void SetDirection(Vector2 newDirection)
-    {
-        direction = newDirection;
+        rigidbody.MovePosition(position + translation);
     }
 }
